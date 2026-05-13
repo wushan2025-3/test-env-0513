186580f089601a8185772343c9c2b06793eaeee6
wushan2025-3 <wushan2025_3@126.com>
2026-05-13 17:06:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
-const html = `<!DOCTYPE html>
-<body>
-  <h1>Hello World</h1>
-</body>`
-
-async function handleRequest(request) {
-  return new Response(html, {
-    headers: {
-      "content-type": "text/html;charset=UTF-8",
-    },
-  })
-}
-
 export default {
-  async fetch(request) {
-    return handleRequest(request);
-  }
+  async fetch(request, context, env) {
+    const TESAT_B = env.TESAT_B;
+
+    return new Response(`Get TEST_KEY_PLAIN: ${TESAT_B} successfully!`);
+  },
 };
