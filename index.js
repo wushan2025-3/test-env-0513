76d9ff919b58202165a717d4bc46d4045dec1668
wushan2025-3 <wushan2025_3@126.com>
2026-07-02 17:39:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ export default {
 
     // ===== 404: Sec-Fetch-Mode: navigate 触发 SPA 兜底 =====
     if (testCase === "404") {
-      const resp = await env.Assets.fetch("http://other.com/notfound.html?_dyc=1", {
+      const resp = await env.Assets.fetch("http://xxx.er.xxxtest.alicdn-test.com/notfound.html?_dyc=1", {
         headers: { "Sec-Fetch-Mode": "navigate" }
       });
       const text = await resp.text();
@@ -161,7 +161,7 @@ export default {
 
     // ===== etag: If-None-Match ETag 匹配 =====
     if (testCase === "etag") {
-      const testUrl1 = "http://xxx.er.xxxtest.alicdn-test.com/test.txt?_dyc=1";
+      const testUrl1 = "http://xxx.er.xxxtest.alicdn-test.com/notfound.html?_dyc=1";
       const testUrl2 = "https://other.com/test.txt?_dyc=1";
       // 先 fetch 资源拿真实 ETag
       const r1 = await env.Assets.fetch(testUrl1);
@@ -191,7 +191,7 @@ export default {
 
     // ===== manual: redirect manual 不跟随重定向 =====
     if (testCase === "manual") {
-      const resp = await env.Assets.fetch("https://xxx.er.xxxtest.alicdn-test.com/sub?_dyc=1", {
+      const resp = await env.Assets.fetch("http://other.com/sub?_dyc=1", {
         redirect: "manual"
       });
       const respHeaders = headersToObj(resp.headers);
