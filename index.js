c2fd956be9b90e235499ac5a814513190bd530df
wushan2025-3 <wushan2025_3@126.com>
2026-07-08 19:40:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ export default {
 
     // ===== 404: Sec-Fetch-Mode: navigate 触发 SPA 兜底 =====
     if (testCase === "404") {
-      const resp = await env.Assets.fetch("182-92-154-134.alicdn-test.com/v2/files/hello.txt", {
+      const resp = await fetch("182-92-154-134.alicdn-test.com/v2/files/hello.txt", {
         headers: { "Sec-Fetch-Mode": "navigate"}
       });
       const text = await resp.text();
