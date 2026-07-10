195c114b44a44672c6370d827d8ad859a1daa454
wushan2025-3 <wushan2025_3@126.com>
2026-07-10 17:05:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 export default {
     async fetch(request, context) {
       return fetch("http://d1d21.ncdn.dns-site029.alicdn-test.com/v2/files/hello_er.txt", {
-        headers: {"host":"d1d21.ncdn.dns-site029.alicdn-test.com"},
+        {host:"d1d21.ncdn.dns-site029.alicdn-test.com"},
       });/*  */
     },
   };
