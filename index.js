149022347175dfa24373656964185ad5b00c6938
wushan2025-3 <wushan2025_3@126.com>
2026-07-10 15:31:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function headersToObj(headers) {
 export default {
   async fetch(request, context, env) {
     const url = new URL(request.url);
-    const subUrl = url.searchParams.get("subUrl") || "https://testcdn.1.alicdn-test.com/v2/files/hello_er.txt";
-    const overrideHost = url.searchParams.get("host") || "realcert.alicdn-test.com";
+    const subUrl = url.searchParams.get("subUrl") || "http://xxx.er.xxxtest.alicdn-test.com/v2/files/hello_er.txt";
+    const overrideHost = url.searchParams.get("host") || "xxx.er.xxxtest.alicdn-test.com";
 
     const fetchOpts = {};
     if (overrideHost) fetchOpts.host = overrideHost;
