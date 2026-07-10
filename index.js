f9fe7f3d8a8feccd8d4ecc42d740ad1b8acec9d1
wushan2025-3 <wushan2025_3@126.com>
2026-07-10 17:11:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 export default {
     async fetch(request, context) {
-      return fetch("http://d1d21.ncdn.dns-site029.alicdn-test.com/v2/files/hello_er.txt", {
-        {host:"d1d21.ncdn.dns-site029.alicdn-test.com"},
+      return fetch("http://xxssx.er.xxxtest.alicdn-test.com/v2/files/hello_er.txt", {
+        {host:"xxssx.er.xxxtest.alicdn-test.com"},
       });/*  */
     },
   };
