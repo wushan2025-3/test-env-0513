4c295f08dd3eae25c6bbc2dd163680823f83c688
wushan2025-3 <wushan2025_3@126.com>
2026-07-10 17:01:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,7 @@
-// ESA EdgeWorker 最简子请求测试脚本
-// 用法：curl 'http://域名/er.js?_dyc=1&subUrl=<子请求URL>&host=<覆盖host>' -x VIP:PORT
-
-function headersToObj(headers) {
-  const obj = {};
-  for (const [k, v] of headers.entries()) obj[k] = v;
-  return obj;
-}
-
 export default {
-  async fetch(request, context, env) {
-    const url = new URL(request.url);
-    const subUrl = url.searchParams.get("subUrl") || "http://d1d21.ncdn.dns-site029.alicdn-test.com/v2/files/hello_er.txt";
-    const overrideHost = url.searchParams.get("host") || "d1d21.ncdn.dns-site029.alicdn-test.com";
-
-    const fetchOpts = {};
-    if (overrideHost) fetchOpts.host = overrideHost;
-
-    const resp = await fetch(subUrl, fetchOpts);
-    const text = await resp.text();
-    const respHeaders = headersToObj(resp.headers);
-
-    console.log("[sub] subUrl:", subUrl, "host:", overrideHost, "finalUrl:", resp.url);
-
-    return new Response(JSON.stringify({
-      subUrl,
-      hostOverride: overrideHost,
-      finalUrl: resp.url,
-      status: resp.status,
-      headers: respHeaders,
-      body: text.substring(0, 500)
-    }, null, 2), { headers: { "content-type": "application/json" } });
-  },
-};
+    async fetch(request, context) {
+      return fetch("http://d1d21.ncdn.dns-site029.alicdn-test.com/v2/files/hello_er.txt", {
+        headers: {"host":"d1d21.ncdn.dns-site029.alicdn-test.com"},
+      });/*  */
+    },
+  };
