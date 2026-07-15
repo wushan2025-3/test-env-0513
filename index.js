0a047acbd159caf8462b27c8b18598b7bdd48871
wushan2025-3 <wushan2025_3@126.com>
2026-07-15 09:48:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function headersToObj(headers) {
 export default {
   async fetch(request, context, env) {
     const url = new URL(request.url);
-    const subUrl = url.searchParams.get("subUrl") || "http://other.er.xxxtest.alicdn-test.com/v2/files/hello_er.txt";
-    const overrideHost = url.searchParams.get("host") || "xxx.er.xxxtest.alicdn-test.com";
+    const subUrl = url.searchParams.get("subUrl") || "http://xxx.er.xxxtest.alicdn-test.com/v2/files/hello_er.txt";
+    const overrideHost = url.searchParams.get("host") || "two.anycast3.xxxtest.alicdn-test.com";
 
     const fetchOpts = {};
     if (overrideHost) fetchOpts.host = overrideHost;
