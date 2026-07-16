663ad05a14d62450315e10d1fc5ae036abde9cb3
wushan2025-3 <wushan2025_3@126.com>
2026-07-16 14:12:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ export default {
   async fetch(request, context, env) {
     const url = new URL(request.url);
     const subUrl = url.searchParams.get("subUrl") || "http://xxx.er.xxxtest.alicdn-test.com/v2/files/hello_er.txt";
-    const overrideHost = url.searchParams.get("host") || "two.anycast3.xxxtest.alicdn-test.com";
+    const overrideHost = url.searchParams.get("host") || "realcert.alicdn-test.com";
 
     const fetchOpts = { redirect: "manual" };
     if (overrideHost) fetchOpts.host = overrideHost;
